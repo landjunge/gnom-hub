2cad5d90e73f7b82e9e11038ea0ae9748a0cc369
Daniel <155759754+landjunge@users.noreply.github.com>
2026-05-19 13:04:02 +0200

    chore: Clean up git repository and remove redundant root agent files from tracking

--- test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('frontend/index.html', 'utf8');
-const scriptMatch = html.match(/<script>([\s\S]*?)<\/script>/g);
-if (scriptMatch) {
-    const scriptContent = scriptMatch[scriptMatch.length - 1].replace(/<script>|<\/script>/g, '');
-    fs.writeFileSync('test_script.js', scriptContent);
-}
